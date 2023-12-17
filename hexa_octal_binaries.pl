ddebe126ec6b6de8b74cab25598d5f4bbbbd56d5
kaleshripad11 <kaleshripad11@gmail.com>
2023-12-17 15:16:22 +0530

    Updated new files

---
+++ hexa_octal_binaries.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+print(0454,"\n");			# Octal
+print(0x96,"\n");			# Hexa
+print(0b111000010,"\n")			# Binary
---
+++ numbers_and_literals.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+# Data that is typed in source code is called as literls in perl
+# Scalers : Numbers, Strings
+
+print(100,"\n");
+print(-200,"\n");
+print(90e50,"\n");
+print(398_980,"\n");
