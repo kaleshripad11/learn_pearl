4a05597a584f998660228e62076e5780560d14a3
kaleshripad11 <kaleshripad11@gmail.com>
2023-12-17 11:34:03 +0530

    Added sample code for 'say' function in Perl

---
+++ say_demo.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+# Define from which perl version features to use
+use feature ':5.30';  
+use strict;
+use warnings;
+
+# Print message using 'say' function
+say "Hey there, printing this line using 'say' function!!";
