4a11b8baf71afe742aef8607ce28f6f2d84c1dc6
kaleshripad11 <kaleshripad11@gmail.com>
2023-12-19 20:22:04 +0530

    Added assignment code

---
+++ Assignment01_literals.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl
+
+print("This is literally a string that ends with the number 7.\n7 7 7 93 11 could be a phone number or the name of a song.\n\nSo could be 637 5309\n\nBut ou812 couldn\\'t be a phone number.\n");
+
